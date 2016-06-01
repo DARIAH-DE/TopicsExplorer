808856c3f87f6c55dc8cbf804a554d508b6f8040
Severin Simmler <severin.simmler@yahoo.com>
2016-06-01 16:59:27 +0200

    Update topics.py
    Added Docstrings and def testing()

--- topics.py
+++ topics.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 ################################################################################
 # Load all dependencies
 ################################################################################
@@ -15,63 +17,130 @@ from gensim import corpora, models, similarities
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
                     level=logging.INFO)
 
+def testing(required_1, required_2, required_3):
+  """
+  Test import libraries.
+
+  Args:
+    required_1: Name of the library, e.g. numpy.
+    required_2: Name of the library, e.g. matplotlib
+    required_3: Name of the library, e.g. gensim
+
+  Returns:
+    Name of the library plus version or error.
+  """
+  
+  try:
+      import required_1
+      import required_2
+      import required_3
+      print(required_1, required_1.__version__, "\n", required_2, required_2.__version__, "\n", required_3, required_3.__version__)
+  except ImportError:
+      print("ERROR: Make sure all required packages are installed.")
+
 ################################################################################
 # Corpus ingestion
 ################################################################################
 
-# Read corpus into a list of lists
 def readCorpus(path):
-    files = glob.glob(path)
-    documents = []
-    for file in files:
-        document = open(file)
-        document = document.read()
-        documents.append(document)
-    return documents
+  """
+  Read corpus into a list of lists.
 
-# Create a list of document labels from file names
+  Args:
+    path: Path to corpus.
+
+  Returns:
+    The list.
+  """
+	
+  files = glob.glob(path)
+  documents = []
+  for file in files:
+    document = open(file)
+    document = document.read()
+    documents.append(document)
+  return documents
+
 def docLabels(path):
-    labels = [os.path.basename(x) for x in glob.glob(path)]
-    labels = [x.split('.')[0] for x in labels]
-    return labels
+  """
+  Create a list of document labels from file names.
+
+  Args:
+    path: Path to corpus.
+
+  Returns:
+    The labels.
+  """
+  
+  labels = [os.path.basename(x) for x in glob.glob(path)]
+  labels = [x.split('.')[0] for x in labels]
+  return labels
 
 ################################################################################
 # Preprocessing
 ################################################################################
 
-# Tokenize texts
 def tokenize(documents):
-    # define regular expression for tokenization
-    myRegEx = re.compile('\w+') # compile regex for fast repetition
-    texts = []
-    for document in documents:
-        text = myRegEx.findall(document.lower())
-        texts.append(text)
-    # Version from Gensim-Tutorial: whithout regex
-    #texts = [[word for word in document.lower().split()]
-    #         for document in documents]
-    return texts
+  """
+  Tokenize text.
 
-# Remove hapax legomena
+  Args:
+    documents: A list of lists containing text.
+
+  Returns:
+    Tokens in a list.
+	"""
+	
+  # define regular expression for tokenization
+  myRegEx = re.compile('\w+') # compile regex for fast repetition
+  texts = []
+  for document in documents:
+    text = myRegEx.findall(document.lower())
+    texts.append(text)
+  # Version from Gensim-Tutorial: whithout regex
+  #texts = [[word for word in document.lower().split()]
+  #         for document in documents]
+  return texts
+
 def removeHapaxLeg(texts):
-    frequency = defaultdict(int)
-    for text in texts:
-        for token in text:
-            frequency[token] += 1
+  """
+	Remove hapax legomena and return text.
+
+  Args:
+    texts: A list of lists containing tokens.
+
+  Returns:
+    Tokens in a list minus hapax legomena.
+	"""
+	
+  frequency = defaultdict(int)
+  for text in texts:
+    for token in text:
+      frequency[token] += 1
     texts = [[token for token in text if frequency[token] > 1]
             for text in texts]
     return texts
 
-# Remove stopwords according to stopword list
 def removeStopWords(texts, stoplist):
-    if isinstance(stoplist, str):
-        file = open('./helpful_stuff/stopwords/' + stoplist)
-        stoplist = file.read()
-        stoplist = [word for word in stoplist.split()]
-        stoplist = set(stoplist)
-    texts = [[word for word in text if word not in stoplist]
+  """
+	Remove stopwords according to stopword list.
+
+  Args:
+    texts: A list of lists containing tokens.
+    stoplist: .txt file containing stopwords.
+
+  Returns:
+    Tokens in a list minus stopwords.
+	"""
+	
+  if isinstance(stoplist, str):
+    file = open('./helpful_stuff/stopwords/' + stoplist)
+    stoplist = file.read()
+    stoplist = [word for word in stoplist.split()]
+    stoplist = set(stoplist)
+  texts = [[word for word in text if word not in stoplist]
              for text in texts]
-    return texts
+  return texts
 
 ################################################################################
 # Gensim model creation
@@ -83,73 +152,120 @@ def gensimModel(texts, # list of tokenized texts
                ldaSource = 'gensim', # 'gensim' or 'mallet'
                mallet_path = '~/Software/mallet/bin/mallet' #future default 'UNKNOWN', or docker solution
                ):
+  """
+	Create model with gensim or mallet.
 
-    # create dictionary and vectorize
-    dictionary = corpora.Dictionary(texts)
-    corpus = [dictionary.doc2bow(text) for text in texts]
+  Args:
+    texts: A list of lists containing tokens.
+    topics: Number of topics, default = 10.
+    ldaSource: gensim or mallet.
+    mallet_path = Path to mallet, default = '~/Software/mallet/bin/mallet'.
 
-    # create a gensim type topic model
-    if ldaSource == 'gensim':
-        model = models.LdaModel(corpus,
-                                id2word=dictionary,
-                                num_topics = topics,
-                                passes = 10
-                               )
-    else:
-        if mallet_path == 'UNKNOWN':
-            mallet_path = '~/Software/mallet/bin/mallet'# TODO: find a function that opens a selection window
-        model = models.wrappers.LdaMallet(
-            mallet_path, # Path to local mallet binary
-            corpus, # Vectorized copus object
-            id2word = dictionary,
-            num_topics = topics, # Number of topics
-            iterations = 100 # Number of iterations in Gibbs sampling
-        )
+  Returns:
+    The model, dictionary, corpus and topics.
+	"""
 
-    # return results
-    return [model, dictionary, corpus, topics] #TODO: store more info about model specifications
+  # create dictionary and vectorize
+  dictionary = corpora.Dictionary(texts)
+  corpus = [dictionary.doc2bow(text) for text in texts]
 
-# Generate topic labels from gensim model
+  # create a gensim type topic model
+  if ldaSource == 'gensim':
+    model = models.LdaModel(corpus,
+                            id2word=dictionary,
+                            num_topics = topics,
+                            passes = 10
+                           )
+  else:
+    if mallet_path == 'UNKNOWN':
+        mallet_path = '~/Software/mallet/bin/mallet'# TODO: find a function that opens a selection window
+    model = models.wrappers.LdaMallet(
+        mallet_path, # Path to local mallet binary
+        corpus, # Vectorized copus object
+        id2word = dictionary,
+        num_topics = topics, # Number of topics
+        iterations = 100 # Number of iterations in Gibbs sampling
+    )
+
+  # return results
+  return [model, dictionary, corpus, topics] #TODO: store more info about model specifications
+
 def topicLabels(model, no_of_topics): #TODO: extract no_of_topics from corpus
-    labels = []
-    for i in range(no_of_topics):
-        terms = [x[0] for x in model.show_topic(i, topn=3)]
-        labels.append(" ".join(terms))
-    return labels
+  """
+  Generate topic labels from model.
 
-# Save all the gensim output
+  Args:
+    model: In gensimModel created.
+    no_of_topics: Number of topics, default = 10.
+
+  Returns:
+    The topic labels.
+	"""
+	
+  labels = []
+  for i in range(no_of_topics):
+    terms = [x[0] for x in model.show_topic(i, topn=3)]
+    labels.append(" ".join(terms))
+  return labels
+
 def saveGensimModel(model,
-    corpus,
-    dictionary,
-    no_of_topics,
-    doc_labels,
-    foldername = 'corpus'
-    ):
-    print("saving ...\n")
-    topics = model.show_topics(num_topics = no_of_topics)
-    if not os.path.exists("out"): os.makedirs("out")
-    with open("out/"+foldername+"_doclabels.txt", "w") as f:
-        for item in doc_labels: f.write(item+"\n")
-    with open("out/"+foldername+"_topics.txt", "w") as f:
-        for item, i in zip(topics, enumerate(topics)):
-            f.write("topic #"+str(i[0])+": "+str(item)+"\n")
-    dictionary.save("out/"+foldername+".dict")
-    corpora.MmCorpus.serialize("out/"+foldername+".mm", corpus)
-    model.save("out/"+foldername+".lda")
+                    corpus,
+                    dictionary,
+                    no_of_topics,
+                    doc_labels,
+                    foldername = 'corpus'
+                    ):
+  """
+	Save all the gensim output in folder "out".
+
+  Args:
+    model: In gensimModel created model.
+    corpus: In gensimModel created corpus.
+    dictionary: In gensimModel created dictionary.
+    no_of_topics: Number of topics, default = 10.
+    doc_labels: In docLabels created labels.
+    foldername: Name of corpus folder, default = corpus.
+
+  Returns:
+    corpus_doclabels.txt, corpus_topics.txt, corpus.dict, corpus.mm, corpus.lda.
+	"""
+	
+  print("saving ...\n")
+  topics = model.show_topics(num_topics = no_of_topics)
+  if not os.path.exists("out"): os.makedirs("out")
+  with open("out/"+foldername+"_doclabels.txt", "w") as f:
+    for item in doc_labels: f.write(item+"\n")
+  with open("out/"+foldername+"_topics.txt", "w") as f:
+    for item, i in zip(topics, enumerate(topics)):
+      f.write("topic #"+str(i[0])+": "+str(item)+"\n")
+  dictionary.save("out/"+foldername+".dict")
+  corpora.MmCorpus.serialize("out/"+foldername+".mm", corpus)
+  model.save("out/"+foldername+".lda")
 
 ################################################################################
 # Doc-Topic matrix
 ################################################################################
 
-# Create a doc-topic matrix from gensim output
 def gensim_to_dtm(model, corpus, no_of_topics):
-    no_of_docs = len(corpus)
-    doc_topic = np.zeros((no_of_docs, no_of_topics))
-    for doc, i in zip(corpus, range(no_of_docs)):   # Use document bow from corpus
-        topic_dist = model.__getitem__(doc)         # to get topic distribution from model
-        for topic in topic_dist:                    # topic_dist is a list of tuples (topic_id, topic_prob)
-            doc_topic[i][topic[0]] = topic[1]       # save topic probability
-    return doc_topic
+  """
+	Create a doc-topic matrix from gensim output.
+
+  Args:
+    model: In gensimModel created model.
+    corpus: In gensimModel created corpus.
+    no_of_topics: Number of topics, default = 10.
+
+  Returns:
+    A doc-topic matrix.
+	"""
+	
+  no_of_docs = len(corpus)
+  doc_topic = np.zeros((no_of_docs, no_of_topics))
+  for doc, i in zip(corpus, range(no_of_docs)):   # Use document bow from corpus
+    topic_dist = model.__getitem__(doc)         # to get topic distribution from model
+    for topic in topic_dist:                    # topic_dist is a list of tuples (topic_id, topic_prob)
+      doc_topic[i][topic[0]] = topic[1]       # save topic probability
+  return doc_topic
 
 ################################################################################
 # Topic visualization
@@ -159,14 +275,25 @@ def gensim_to_dtm(model, corpus, no_of_topics):
 #Not convinient on MS Windows, pip installation on Ubuntu failed too
 #http://nlp.stanford.edu/events/illvi2014/papers/sievert-illvi2014.pdf
 
-# Document-topic heatmap
 def docTopHeatmap(doc_topic, doc_labels, topic_labels):
-    no_of_topics = len(doc_labels)
-    if no_of_topics > 20 or no_of_topics > 20: plt.figure(figsize=(20,20))    # if many items, enlarge figure
-    plt.pcolor(doc_topic, norm=None, cmap='Reds')
-    plt.yticks(np.arange(doc_topic.shape[0])+1.0, doc_labels)
-    plt.xticks(np.arange(doc_topic.shape[1])+0.5, topic_labels, rotation='90')
-    plt.gca().invert_yaxis()
-    plt.colorbar(cmap='Reds')
-    plt.tight_layout()
-    plt.show()
+  """
+	Create document-topic heatmap.
+
+  Args:
+    doc_topic: In gensim_to_dtm created doc-topic matrix.
+    doc_labels: In docLabels created labels.
+    topic_labels: In topicLabels created labels.
+
+  Returns:
+    A heatmap saved as .png 
+	"""
+	
+  no_of_topics = len(doc_labels)
+  if no_of_topics > 20 or no_of_topics > 20: plt.figure(figsize=(20,20))    # if many items, enlarge figure
+  plt.pcolor(doc_topic, norm=None, cmap='Reds')
+  plt.yticks(np.arange(doc_topic.shape[0])+1.0, doc_labels)
+  plt.xticks(np.arange(doc_topic.shape[1])+0.5, topic_labels, rotation='90')
+  plt.gca().invert_yaxis()
+  plt.colorbar(cmap='Reds')
+  plt.tight_layout()
+  plt.show()
