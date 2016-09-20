82de93187e3cdaeeab5f3aa8bae4555491c777f4
severinsimmler <severin.simmler@yahoo.com>
2016-09-20 10:29:39 +0200

    Some more updates

--- Jinja2/engine.py
+++
@@ -1,7 +0,0 @@
-from jinja2 import Template
-
-with open('index.html.jinja') as f:
-    tmpl = Template(f.read())
-
-with open('ergebnis.html', 'w') as f:
-    f.write(tmpl.render(title = 'Demonstrator', item_list = ["Erster Punkt", "Zweiter Punkt", "Dritter Punkt"]))
