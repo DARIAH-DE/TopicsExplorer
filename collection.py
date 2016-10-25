6f172135e44d074b51b1055b8779feac61a8cd53
Severin Simmler <simmler@posteo.de>
2016-10-25 10:28:23 +0200

    Update logging

--- collection.py
+++ collection.py
@@ -17,7 +17,7 @@ __author__ = "DARIAH-DE"
 __authors__ = "Stefan Pernes, Steffen Pielstroem, Philip Duerholt, Sina Bock, Severin Simmler"
 __email__ = "stefan.pernes@uni-wuerzburg.de, pielstroem@biozentrum.uni-wuerzburg.de"
 __version__ = "0.3"
-__date__ = "2016-10-24"
+__date__ = "2016-10-25"
 
 import pandas as pd
 import re
@@ -43,43 +43,43 @@ def create_document_list(path, ext='.txt'):
 
     Args:
         path (str): Path to folder, e.g. '/tmp/corpus'.
-        suffix (str): File extension, e.g. '.csv'. Defaults to '.txt'.
+        ext (str): File extension, e.g. '.csv'. Defaults to '.txt'.
 
     Returns:
         list[str]: List of files with full path.
     """
     
+    log.info("Creating document list ...")
     doclist = glob.glob(path + "/*" + ext)
+    log.debug("Document list successfully created.")
     return doclist
-
 
 def read_from_txt(doclist):
     """Opens files using a list of paths.
 
     Note:
-        Use `create_document_list()` to create instance attributes.
+        Use `create_document_list()` to create `doclist`.
         
     Args:
-        Doclist (list[str]): List of all documents in the corpus.
+        doclist (list[str]): List of all documents in the corpus.
         
     Yields:
         Documents in `doclist`.
 
     Todo:
-
         Seperate metadata (author, header)?
     """
-
+    log.info("Accessing documents ...")
     for file in doclist:
         with open(file, 'r', encoding = 'utf-8') as f:
             yield f.read()
-            
+    log.debug("Documents successfully accessed.")
 
 def segmenter(doc, length):
     """Segments documents.
 
     Note:
-        Use `ReadFromTXT` to create instance attributes.
+        Use `ReadFromTXT` to create `doc`.
         
     Args:
         doc (str): Document as iterable.
@@ -91,12 +91,12 @@ def segmenter(doc, length):
     Todo:
         Implement fuzzy option to consider paragraph breaks.              
     """
-
+    log.info("Segmenting documents ...")
     doc = doc.split()
     for i, word in enumerate(doc):
         if i % length == 0:
             yield doc[i : i + length]
-
+    log.debug("Documents successfully segmented.")
 
 def filter_POS_tags(doclist_csv, pos_tags, columns):
     """Gets selected POS-tags from DKPro-Wrapper output.
@@ -113,39 +113,40 @@ def filter_POS_tags(doclist_csv, pos_tags, columns):
         columns (List): List of DKPro columns that should be selected?
         Default values for parameters
         readCSV-function?
+        Parameter `file` missing?!?
     """
-
+    
+    log.info("Accessing selected POS-tags ...")
     #pos_tags = ['ADJ', "V", "NN"]
     #columns = ['ParagraphId', 'TokenId', 'Lemma', 'CPOS', 'NamedEntity']
     df = pd.DataFrame()
-    
     df = pd.read_csv(file, sep="\t", quoting=csv.QUOTE_NONE)
     df = df[columns]
 
     for p in pos_tags:
        df = df.loc[df["CPOS"] == p]
        yield df.loc[df["CPOS"] == p]["Lemma"]
-    
+    log.debug("POS-tags successfully accessed.")
 
 def get_labels(files):
     """Gets document labels.
     
     Args:
-        
+        files (list[str]): List of file paths.
 
     Yields:
         Iterable: Document labels.
     """
+    
+    log.info("Creating document labels ...")
     labels = []
-        
     for file in files:
         label = os.path.basename(file)
         yield label
-
+    log.debug("Document labels successfully created.")
 
 class Visualization:
     def __init__(self, lda_model, corpus, dictionary, doc_labels, interactive):
-
         """Loads Gensim output for further processing.
 
         The output folder should contain ``corpus.mm``, ``corpus.lda``, as well as
@@ -165,31 +166,32 @@ class Visualization:
             OSError: If directory or files not found.
             ValueError: If no matching values found.
             Unexpected error: Everything else.
-
-        Author:
-            DARIAH-DE
         """
         try:
             log.info("Accessing corpus ...")
             self.corpus = MmCorpus(corpus)
+            log.debug("Corpus successfully accessed.")
 
             log.info("Accessing model ...")
             self.model = LdaModel.load(lda_model)
+            log.debug("Model successfully accessed.")
 
             if interactive == False:
                 log.debug("`interactive` set to False.")
                 log.info("Accessing doc_labels ...")
                 self.doc_labels = doc_labels
+                log.debug("doc_labels successfully accessed.")
                 with open(self.doc_labels, 'r', encoding='utf-8') as f:
                     self.doc_labels = [line for line in f.read().split()]
                     log.debug("Accessed %s doc_labels.", len(doc_labels))
-                log.info("Successfully created corpus, model, doc_labels for heatmap visualization.")
+                log.info("Corpus, model, doc_labels for heatmap visualization successfully created.")
 
             elif interactive == True:
                 log.debug("`interactive` set to True.")
                 log.info("Accessing dictionary ...")
                 self.dictionary = Dictionary.load(dictionary)
-                log.info("Successfully created corpus, model, dictionary for interactive visualization.")
+                log.debug("Dictionary successfully accessed.")
+                log.info("Corpus, model, dictionary for interactive visualization successfully created.")
 
         except OSError as err:
             log.error("OS error: {0}".format(err))
@@ -226,17 +228,19 @@ class Visualization:
         log.info("Loading topic distribution from model ...")
         for doc, i in zip(self.corpus, range(no_of_docs)):
             topic_dist = self.model.__getitem__(doc)
+        log.debug("Topic distribution successfully loaded from model.")
 
         log.info("Saving topic probability ...")
         for topic in topic_dist: # topic_dist is a list of tuples (topic_id, topic_prob)
             doc_topic[i][topic[0]] = topic[1]
+        log.debug("Topic probability successfully saved.")
 
         log.info("Loading plot labels ...")
         topic_labels = []
         for i in range(no_of_topics):
             topic_terms = [x[0] for x in self.model.show_topic(i, topn=3)] # show_topic() returns tuples (word_prob, word)
             topic_labels.append(" ".join(topic_terms))
-        log.debug("Saved %s topic_labels.", len(self.topic_labels))
+        log.debug("Saved %s topic_labels.", len(topic_labels))
 
         log.info("Creating heatmap figure ...")
         if no_of_docs > 20 or no_of_topics > 20:
@@ -247,7 +251,7 @@ class Visualization:
         plt.gca().invert_yaxis()
         plt.colorbar(cmap='Reds')
         heatmap = plt.tight_layout()
-        log.info("Successfully created heatmap figure.")
+        log.debug("Heatmap figure successfully created.")
         return heatmap
 
     def save_heatmap(self, path):
@@ -263,9 +267,9 @@ class Visualization:
         Returns:
             ~/out/corpus_heatmap.png
         """
-        log.info("Saving heatmap ...")
+        log.info("Saving heatmap figure...")
         plt.savefig(os.path.join(path, 'corpus_heatmap.png'), dpi= 200)
-        log.info("Successfully saved heatmap as corpus_heatmap.png")
+        log.debug("Heatmap figure successfully saved as corpus_heatmap.png")
 
     def make_interactive(self):
         """Generates interactive visualization from LDA model.
@@ -283,7 +287,7 @@ class Visualization:
         """
         log.info("Loading model, corpus, dictionary ...")
         vis = pyLDAvis.gensim.prepare(self.model, self.corpus, self.dictionary)
-        log.info("Successfully created interactive visualization.")
+        log.debug("Interactive visualization successfully created.")
         return vis
 
 
@@ -305,4 +309,4 @@ class Visualization:
         pyLDAvis.save_html(vis, os.path.join(path, 'corpus_interactive.html'))
         pyLDAvis.save_json(vis, os.path.join(path, 'corpus_interactive.json'))
         pyLDAvis.prepared_data_to_html(vis)
-        log.info("Successfully saved interactive visualization.")
+        log.info("Interactive visualization successfully saved as corpus_interactive.html and corpus_interactive.json.")
