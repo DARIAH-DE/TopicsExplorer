a51fb6a4a1d1221a66b903e5008a0313e0fca48e
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2017-01-20 18:00:08 +0100

    Basic package structure

---
+++ dariah_topics/__init__.py
@@ -0,0 +1,4 @@
+"""
+The `dariah_topics` package currently offers one module,
+`dariah_topics.collection`.
+"""
---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name='dariah_topics',
+    version='0.2.0dev',
+    description='DARIAH Topic Modelling',
+    # url
+    author="DARIAH-DE Wuerzburg Group",
+    author_email="pielstroem@biozentrum.uni-wuerzburg.de",
+    # license
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ],
+    # keywords
+    packages=find_packages(exclude=['corpus_*', 'docs', 'tests']),
+    install_requires=[
+        'pandas>=0.19.2',
+        'regex>=2017.01.14',
+        'gensim>=0.13.2',
+        'matplotlib==1.5.3',
+        'numpy>=1.3',
+        'scipy>=0.7',
+        'werkzeug>=0.11.15',  # to feature 'demonstrator'
+        'flask>=0.11.1',      # to feature 'demonstrator'
+        'pyLDAvis>=2.0.0',    # to feature 'pyldavis'
+    ])
