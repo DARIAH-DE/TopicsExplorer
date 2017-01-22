40aa90f03288c5f79b6083279549617a817e4d94
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2017-01-22 16:47:50 +0100

    Moved modules to dariah_topics package

--- dariah_topics/__init__.py
+++ dariah_topics/__init__.py
@@ -1,4 +1,7 @@
 """
-The `dariah_topics` package currently offers one module,
-`dariah_topics.collection`.
+The `dariah_topics` package currently offers three modules:
+
+* `dariah_topics.preprocessing` contains preprocessing code
+* `dariah_topics.visualization` offers visualization stuff that depends on PyLDAvis
+* `dariah_topics.mallet` provides a wrapper that calls mallet
 """
