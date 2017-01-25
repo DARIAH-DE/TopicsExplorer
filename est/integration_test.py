3d520eee095e1d4714f94f5827fee15683473197
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2017-01-25 21:40:15 +0100

    Channeled nbconvert output a little

--- est/integration_test.py
+++ est/integration_test.py
@@ -1,6 +1,7 @@
-from subprocess import check_call
+from subprocess import check_output, STDOUT, CalledProcessError
 from nose.plugins.skip import SkipTest
 from pathlib import Path
+import logging
 
 project_path = Path(__file__).absolute().parent.parent
 
@@ -10,8 +11,12 @@ def jupyter_integration_test():
     Tries to run the integration test notebook using jupyter.
     """
     try:
-        check_call(["jupyter-nbconvert", "--execute",
-                    str(Path(project_path, "IntegrationTest_v01.ipynb"))])
+        check_output(["jupyter-nbconvert", "--execute",
+                    str(Path(project_path, "IntegrationTest_v01.ipynb"))],
+                     stderr=STDOUT, universal_newlines=True)
     except FileNotFoundError as e:
         raise SkipTest("jupyter-nbconvert not found. Cannot run integration test. "
                    + str(e))
+    except CalledProcessError as e:
+        logging.error(e.output)
+        raise
