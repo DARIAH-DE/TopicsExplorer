594552bec0c3ea65216edee238d72a7b25d95aea
Severin Simmler <severin@localhost.localdomain>
2017-02-03 22:12:46 +0100

    Repo clean up

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name='dariah_topics',
+    version='0.2.0dev0',
+    description='DARIAH Topic Modelling',
+    # url
+    author="DARIAH-DE Wuerzburg Group",
+    author_email="pielstroem@biozentrum.uni-wuerzburg.de",
+    # license
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ],
+    # keywords
+    packages=find_packages(exclude=['corpus_*', 'docs', 'tests']),
+    install_requires=[
+        'pandas>=0.19.2',
+        'regex>=2017.01.14',
+        'gensim>=0.13.2',
+        'matplotlib==1.5.3',
+        'numpy>=1.3',
+        'scipy>=0.7',
+    ],
+    # pip install -e .[demonstrator,vis,evaluation]
+    extras_require={
+        'demonstrator': [
+            'werkzeug>=0.11.15',
+            'flask>=0.11.1'
+        ],
+        'vis': [
+            'pyLDAvis>=2.0.0',    # to feature 'pyldavis'
+        ],
+        'evaluation': [
+            'wikipedia>=1.4.0',
+            'lxml>=3.6.4'
+        ]
+    }
+)
