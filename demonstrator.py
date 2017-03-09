6b74cc16918db566549d2babfa358693d69b3237
Severin Simmler <severin@localhost.localdomain>
2017-03-09 15:20:09 +0100

    Demonstrator stuff

--- demonstrator.py
+++ demonstrator.py
@@ -15,9 +15,9 @@ Todo: Replace print statements with logging (which is currently not working).
 
 import matplotlib
 matplotlib.use('Agg')
-from dariah_topics import preprocessing
-from dariah_topics import visualization
-from dariah_topics import mallet
+from dariahs_topics import preprocessing
+from dariahs_topics import visualization
+from dariahs_topics import mallet
 from flask import Flask, request, render_template, send_file
 from gensim.models import LdaModel
 from gensim.corpora import MmCorpus
@@ -28,6 +28,8 @@ import pandas as pd
 import shutil
 import threading
 import webbrowser
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 from werkzeug.utils import secure_filename
 
 __author__ = "Severin Simmler"
@@ -43,57 +45,106 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    # access input
     files = request.files.getlist('files')
     lda = request.form['lda']
+    num_topics = int(request.form['number_topics'])
+    num_iterations = int(request.form['number_iterations'])
+    threshold = int(request.form['mfws'])
+    
     if 'gensim' in lda:
         corpus = pd.Series()
+
     print("Accessing and tokenizing files ...")
     for file in files:
         filename, extension = os.path.splitext(secure_filename(file.filename))
+        
         if 'mallet' in lda:
             os.makedirs('./tmp_files', exist_ok=True)
-            file.save("./tmp_files/" + secure_filename(file.filename))
-        else:
             if extension == '.txt':
-                text = file.read().decode('utf-8')
-                file.flush()
+                file.save('./tmp_files/' + secure_filename(file.filename))
             elif extension == '.xml':
                 ns = dict(tei="http://www.tei-c.org/ns/1.0")
                 text = etree.parse(file)
                 text = text.xpath('//tei:text', namespaces=ns)[0]
                 text = "".join(text.xpath('.//text()'))
-                file.flush()
+                with open('./tmp_files/' + secure_filename(file.filename), 'w+', encoding='utf-8') as f:
+                    f.writelines(text)
+            
+        
+        elif 'gensim' in lda:
+            if extension == '.txt':
+                text = file.read().decode('utf-8')
+            elif extension == '.xml':
+                ns = dict(tei="http://www.tei-c.org/ns/1.0")
+                text = etree.parse(file)
+                text = text.xpath('//tei:text', namespaces=ns)[0]
+                text = "".join(text.xpath('.//text()'))
             else:
-                print("File format is not supported.")
+                print("Error: File format is not supported.")
             tokens = list(preprocessing.tokenize(text))
             label = filename
             corpus[label] = tokens
+        file.flush()
+
     if 'mallet' in lda:
         print("Creating MALLET binary ...")
-        mallet.create_mallet_model("./mallet_output", "./tmp_files", './mallet/bin/mallet')
+        if request.files.get('stoplist', None):
+            os.makedirs('./stopwordlist', exist_ok=True)
+            stopwords = request.files['stoplist']
+            stopwords.save('./stopwordlist/' + secure_filename(stopwords.filename))
+            try:
+                mallet.create_mallet_model("./mallet_output", "./tmp_files", 'mallet', stoplist='./stopwordlist/' + secure_filename(stopwords.filename))
+            except:
+                mallet.create_mallet_model("./mallet_output", "./tmp_files", './mallet/bin/mallet', stoplist='./stopwordlist/' + secure_filename(stopwords.filename))
+            shutil.rmtree('./stopwordlist')
+        else:
+            try:
+                mallet.create_mallet_model("./mallet_output", "./tmp_files", 'mallet')
+            except:
+                mallet.create_mallet_model("./mallet_output", "./tmp_files", './mallet/bin/mallet')
+            
         print("Training MALLET LDA model ...")
-        mallet.create_mallet_output('./mallet_output/malletModel.mallet', './mallet_output', './mallet/bin/mallet')
-        shutil.rmtree('./tmp_files')
-        df = mallet.show_topics_keys('./mallet_output')
+        try:
+            mallet.create_mallet_output('./mallet_output/malletModel.mallet', './mallet_output', 'mallet', num_topics=str(num_topics), num_iterations=str(num_iterations))
+        except:
+            mallet.create_mallet_output('./mallet_output/malletModel.mallet', './mallet_output', './mallet/bin/mallet', num_topics=str(num_topics), num_iterations=str(num_iterations))
+        df = mallet.show_topics_keys('./mallet_output', num_topics=num_topics)
         doc_topic = mallet.show_docTopicMatrix('./mallet_output')
         heatmap = visualization.doc_topic_heatmap(doc_topic)
         heatmap.savefig('./static/heatmap.png')
+        heatmap.close()
+       
+
+        with open ('./mallet_output/topic_keys.txt', 'r', encoding='utf-8') as f:
+            text = f.read()
+            wordcloud = WordCloud(width=800, height=600, background_color='white').generate(text)
+            plt.imshow(wordcloud)
+            plt.axis("off")
+            plt.savefig('./static/cloud.png')
+            plt.close()
+        shutil.rmtree('./tmp_files')
+        shutil.rmtree('./mallet_output')
         print("Rendering result.hml ...")
         return render_template('result.html', tables=[df.to_html(classes='df')])
-    else:
+    
+    elif 'gensim' in lda:
+        labels = corpus.index.tolist()
+        tokens = corpus.tolist()
         print("Creating bag-of-words model ...")
-        id_types, doc_ids = preprocessing.create_dictionaries(corpus.index.tolist(), corpus.tolist())
-        sparse_bow = preprocessing.create_mm(corpus.index.tolist(), corpus.tolist(), id_types, doc_ids)
+        id_types, doc_ids = preprocessing.create_dictionaries(labels, tokens)
+        sparse_bow = preprocessing.create_mm(labels, tokens, id_types, doc_ids)
 
-        stopwords = request.files['stoplist']
         if request.files.get('stoplist', None):
             print("Accessing external stopword list and cleaning corpus ...")
+            stopwords = request.files['stoplist']
             words = stopwords.read().decode('utf-8')
             words = set(preprocessing.tokenize(words))
-            sparse_bow = preprocessing.remove_features(sparse_bow, id_types, words)
+            hapax = preprocessing.find_hapax(sparse_bow, id_types)
+            feature_list = words.union(hapax)
+            sparse_bow = preprocessing.remove_features(sparse_bow, id_types, feature_list)
             stopwords.flush()
         else:
-            threshold = int(request.form['mfws'])
             print("Accessing", threshold, "most frequent words and cleaning corpus ...")
             stopwords = preprocessing.find_stopwords(sparse_bow, id_types, threshold)
             hapax = preprocessing.find_hapax(sparse_bow, id_types)
@@ -107,21 +158,26 @@ def upload_file():
         doc2id = {value : key for key, value in doc_ids.items()}
         type2id = {value : key for key, value in id_types.items()}
 
-        num_topics = int(request.form['number_topics'])
-        passes = int(request.form['passes'])
+        
         print("Training Gensim LDA with", num_topics, "topics ...")
-        model = LdaModel(corpus=mm, id2word=type2id, num_topics=num_topics, passes=10, iterations=20)
+        model = LdaModel(corpus=mm, id2word=type2id, num_topics=num_topics, iterations=num_iterations, passes=10)
 
         print("Visualizing document-topic matrix and saving as heatmap.png ...")
         doc_topic = visualization.create_doc_topic(mm, model, corpus.index.tolist())
         heatmap = visualization.doc_topic_heatmap(doc_topic)
         heatmap.savefig('./static/heatmap.png')
+        heatmap.close()
+
+        wordcloud = WordCloud(width=800, height=600, background_color='white').fit_words(model.show_topic(1,100))
+        plt.imshow(wordcloud)
+        plt.axis("off")
+        plt.savefig('./static/cloud.png')
+        plt.close()
 
         # Todo: replace by DataFrame.to_html():
         print("Accessing topics for HTML table ...")
-        df = visualization.topicwords_in_df(model)
+        df = preprocessing.gensim2dataframe(model)
         print("Rendering result.html ...")
-
         return render_template('result.html', tables=[df.to_html(classes='df')])
     
     
