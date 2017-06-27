d3f97b1341617d1396d4cc6dfe576fcf54577abb
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2017-06-27 16:36:22 +0200

    Split notebook tests to different test functions

--- est/integration_test.py
+++ est/integration_test.py
@@ -6,85 +6,34 @@ import re
 
 project_path = Path(__file__).absolute().parent.parent
 
+def run_notebook(notebook_name):
+    try:
+        check_output(["jupyter-nbconvert", "--execute",
+                     "--log-level=ERROR",
+                     "--ExecutePreprocessor.iopub_timeout=30",
+                      "--ExecutePreprocessor.timeout=None",
+                    str(Path(project_path, notebook_name))],
+                     stderr=STDOUT, universal_newlines=True)
+    except FileNotFoundError as e:
+        raise SkipTest("jupyter-nbconvert not found. Cannot run integration test. "
+                   + str(e))
+    except CalledProcessError as e:
+        message = e.output
+        cellinfo = re.search('nbconvert.preprocessors.execute.CellExecutionError: (.+)$',
+                  message, re.MULTILINE | re.DOTALL)
+        if cellinfo:
+            message = cellinfo.group(1)
+        logging.error(message)
+
+@SkipTest
+def jupyter_lda_test():
+    run_notebook("Introducing_lda.ipynb")
+
+def jupyter_gensim_test():
+    run_notebook("Introducing_gensim.ipynb")
+
+def jupyter_MALLET_test():
+    run_notebook("Introducing_MALLET.ipynb")
 
 def jupyter_integration_test():
-    """
-    Integration test notebook (via Jupyter)
-    """
-    #Test Introducing_lda.ipynb
-    try:
-        check_output(["jupyter-nbconvert", "--execute",
-                     "--log-level=ERROR",
-                     "--ExecutePreprocessor.iopub_timeout=30",
-                      "--ExecutePreprocessor.timeout=None",
-                    str(Path(project_path, "Introducing_lda.ipynb"))],
-                     stderr=STDOUT, universal_newlines=True)
-    except FileNotFoundError as e:
-        raise SkipTest("jupyter-nbconvert not found. Cannot run integration test. "
-                   + str(e))
-    except CalledProcessError as e:
-        message = e.output
-        cellinfo = re.search('nbconvert.preprocessors.execute.CellExecutionError: (.+)$',
-                  message, re.MULTILINE | re.DOTALL)
-        if cellinfo:
-            message = cellinfo.group(1)
-        logging.error(message)
-
-    #Test Introducing_gensim.ipynb
-    try:
-        check_output(["jupyter-nbconvert", "--execute",
-                     "--log-level=ERROR",
-                     "--ExecutePreprocessor.iopub_timeout=30",
-                      "--ExecutePreprocessor.timeout=None",
-                    str(Path(project_path, "Introducing_gensim.ipynb"))],
-                     stderr=STDOUT, universal_newlines=True)
-    except FileNotFoundError as e:
-        raise SkipTest("jupyter-nbconvert not found. Cannot run integration test. "
-                   + str(e))
-    except CalledProcessError as e:
-        message = e.output
-        cellinfo = re.search('nbconvert.preprocessors.execute.CellExecutionError: (.+)$',
-                  message, re.MULTILINE | re.DOTALL)
-        if cellinfo:
-            message = cellinfo.group(1)
-        logging.error(message)
-
-    #Test Introducing_MALLET.ipynb
-    try:
-        check_output(["jupyter-nbconvert", "--execute",
-                     "--log-level=ERROR",
-                     "--ExecutePreprocessor.iopub_timeout=30",
-                      "--ExecutePreprocessor.timeout=None",
-                    str(Path(project_path, "Introducing_MALLET.ipynb"))],
-                     stderr=STDOUT, universal_newlines=True)
-    except FileNotFoundError as e:
-        raise SkipTest("jupyter-nbconvert not found. Cannot run integration test. "
-                   + str(e))
-    except CalledProcessError as e:
-        message = e.output
-        cellinfo = re.search('nbconvert.preprocessors.execute.CellExecutionError: (.+)$',
-                  message, re.MULTILINE | re.DOTALL)
-        if cellinfo:
-            message = cellinfo.group(1)
-        logging.error(message)
-        raise
-
-    #Test IntegrationTest_txt_gensim.ipynb
-    try:
-        check_output(["jupyter-nbconvert", "--execute",
-                     "--log-level=ERROR",
-                     "--ExecutePreprocessor.iopub_timeout=30",
-                      "--ExecutePreprocessor.timeout=None",
-                    str(Path(project_path, "IntegrationTest_txt_gensim.ipynb"))],
-                     stderr=STDOUT, universal_newlines=True)
-    except FileNotFoundError as e:
-        raise SkipTest("jupyter-nbconvert not found. Cannot run integration test. "
-                   + str(e))
-    except CalledProcessError as e:
-        message = e.output
-        cellinfo = re.search('nbconvert.preprocessors.execute.CellExecutionError: (.+)$',
-                  message, re.MULTILINE | re.DOTALL)
-        if cellinfo:
-            message = cellinfo.group(1)
-        logging.error(message)
-        raise
+    run_notebook("IntegrationTest_txt_gensim.ipynb")
