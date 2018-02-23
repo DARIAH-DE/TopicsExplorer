461fbac20ed3ecf5d7c42de7702844349c313f35
severinsimmler <severin.simmler@posteo.de>
2018-02-23 08:08:52 +0100

    Repair templates

--- bokeh_templates/try_run.js
+++ bokeh_templates/try_run.js
@@ -1,8 +1,7 @@
 (function(root) {
   function embed_document(root) {
-    { % block code_to_run %
-    } { % endblock %
-    }
+    {% block code_to_run %}
+    {% endblock %}
   }
   if (root.Bokeh !== undefined) {
     embed_document(root);
