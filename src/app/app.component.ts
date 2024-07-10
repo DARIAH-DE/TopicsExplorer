befa29991e4356932e321c0c8c9c8a2ef125879a
severinsimmler <s.simmler@snapaddy.com>
2024-07-10 12:29:22 +0200

    fix: comma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,6 @@ import { HomeComponent } from './pages/home/home.component';
   standalone: true,
   imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {}
