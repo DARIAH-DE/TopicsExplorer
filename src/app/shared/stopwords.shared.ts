e792b093f9d391b9b37b53cc664400fcd4620458
severinsimmler <s.simmler@snapaddy.com>
2024-07-30 18:15:11 +0200

    feat: add stopwords

---
+++ src/app/shared/stopwords.shared.ts
@@ -0,0 +1,13 @@
+export const ENGLISH_STOPWORDS = new Set([]);
+export const FRENCH_STOPWORDS = new Set([]);
+export const GERMAN_STOPWORDS = new Set([]);
+export const ITALIAN_STOPWORDS = new Set([]);
+export const SPANISH_STOPWORDS = new Set([]);
+
+export const STOPWORDS = new Set([
+  ...ENGLISH_STOPWORDS,
+  ...FRENCH_STOPWORDS,
+  ...GERMAN_STOPWORDS,
+  ...ITALIAN_STOPWORDS,
+  ...SPANISH_STOPWORDS,
+]);
