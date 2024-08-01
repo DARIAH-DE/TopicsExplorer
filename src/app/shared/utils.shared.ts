9b2a7ea92b544c61381a7efaaf47f9174fb70d2b
severinsimmler <s.simmler@snapaddy.com>
2024-08-01 12:18:39 +0200

    feat: most common words

--- src/app/shared/utils.shared.ts
+++ src/app/shared/utils.shared.ts
@@ -10,6 +10,17 @@ export function tokenizeText(text: string): string[] {
 }
 
 /**
+ * Gets the most common words in a vocabulary.
+ */
+export function getMostCommonWords(counts: Map<string, number>, n: number = 10): Set<string> {
+  return new Set(
+    Array.from(counts.keys())
+      .sort((a, b) => counts.get(b)! - counts.get(a)!)
+      .slice(0, n),
+  );
+}
+
+/**
  * Extracts the vocabulary (a map from token to ID) from a corpus of text documents.
  *
  * @note Also filters out stopwords, single character tokens and hapax legomena.
@@ -23,9 +34,10 @@ export function extractVocabulary(corpus: TextDocument[]): Map<string, number> {
     }
   }
 
+  const mostCommonWords = getMostCommonWords(counts);
   const vocabulary = new Map<string, number>();
   for (const [token, count] of counts.entries()) {
-    if (count > 1) {
+    if (count > 1 && !mostCommonWords.has(token)) {
       vocabulary.set(token, vocabulary.size);
     }
   }
