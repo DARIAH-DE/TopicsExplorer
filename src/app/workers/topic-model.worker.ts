09bd8cd0a94b2009d17b436322b50f00edd050d0
severinsimmler <s.simmler@snapaddy.com>
2024-09-10 18:28:58 +0200

    feat: comments

--- src/app/workers/topic-model.worker.ts
+++ src/app/workers/topic-model.worker.ts
@@ -1,18 +1,23 @@
 /// <reference lib="webworker" />
 
-import { TextCorpus, TopicModelOptions, WorkerMessage } from '../shared/interfaces.shared';
+import { TextCorpus, TopicModelOptions } from '../shared/interfaces.shared';
 import { TopicModel } from '../shared/topic-model.shared';
 
-addEventListener('message', (message: MessageEvent<{ textCorpus: TextCorpus; options: TopicModelOptions }>) => {
+addEventListener('message', onTrainTopicModel);
+
+/**
+ * Trains a topic model using the given text corpus and options.
+ *
+ * @param message The message containing the text corpus and options.
+ */
+export function onTrainTopicModel(message: MessageEvent<{ textCorpus: TextCorpus; options: TopicModelOptions }>): void {
   const model = new TopicModel(message.data.textCorpus, message.data.options);
 
-  console.error("START TRAINING")
-
   for (let i = 0; i < message.data.options.numIterations; i++) {
-    console.error(i)
     model.update();
-    postMessage({ currentIteration: i + 1 });
+
+    postMessage({ currentIteration: i + 1, topics: model.getTopics(5) });
   }
 
   postMessage({ currentIteration: message.data.options.numIterations, model });
-});
+}
