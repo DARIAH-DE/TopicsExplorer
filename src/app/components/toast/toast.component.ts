1166b59564fa0b85b8d85353baa2cefb31ba78b8
severinsimmler <s.simmler@snapaddy.com>
2024-09-17 18:02:39 +0200

    feat: show toast

--- src/app/components/toast/toast.component.ts
+++ src/app/components/toast/toast.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+
+type ToastType = 'info' | 'success' | 'warning' | 'danger';
 
 @Component({
   selector: 'app-toast',
@@ -7,21 +9,33 @@ import { Component, Input } from '@angular/core';
   standalone: true,
 })
 export class ToastComponent {
-  @Input() message: string = '';
-  @Input() type: 'info' | 'success' | 'warning' | 'danger' = 'info';
+  public message: string = '';
+  public type: ToastType = 'info';
 
+  /**
+   * True if the toast is an info message.
+   */
   public get isInfo(): boolean {
     return this.type === 'info';
   }
 
+  /**
+   * True if the toast is a success message.
+   */
   public get isSuccess(): boolean {
     return this.type === 'success';
   }
 
+  /**
+   * True if the toast is a warning message.
+   */
   public get isWarning(): boolean {
     return this.type === 'warning';
   }
 
+  /**
+   * True if the toast is a danger message.
+   */
   public get isDanger(): boolean {
     return this.type === 'danger';
   }
