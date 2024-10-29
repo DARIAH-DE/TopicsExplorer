cb756913ea9310f0c025a42c07621be08c70d982
severinsimmler <s.simmler@snapaddy.com>
2024-10-29 17:54:33 +0100

    feat: app componetn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, inject, viewChild, ViewContainerRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { faDownload, faLayerGroup, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
-import { HomeComponent } from './pages/home/home.component';
-import { ModelComponent } from './pages/model/model.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ToastService } from './services/toast.service';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [FaIconComponent, RouterOutlet, HomeComponent, ModelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [NavbarComponent, RouterOutlet],
+  standalone: true,
 })
-export class AppComponent {
-  public faDownload = faDownload;
-  public faLayerGroup = faLayerGroup;
-  public faSun = faSun;
-  public faMoon = faMoon;
+export class AppComponent implements AfterViewInit {
+  #toastService = inject(ToastService);
 
-  #theme: 'dark' | 'light' = 'dark';
+  public readonly toastContainer = viewChild.required('toastContainer', { read: ViewContainerRef });
 
   /**
-   * Toggles the theme between light and dark mode.
+   * Sets the view container reference for the toast service.
    */
-  public toggleTheme() {
-    this.#theme = this.isDarkTheme ? 'light' : 'dark';
-    const root = document.getElementById('root');
-    if (root) {
-      root.setAttribute('data-theme', this.#theme);
-    }
-  }
-
-  /**
-   * True if the current theme is dark.
-   */
-  public get isDarkTheme(): boolean {
-    return this.#theme === 'dark';
-  }
-
-  /**
-   * True if the current theme is light.
-   */
-  public get isLightTheme(): boolean {
-    return this.#theme === 'light';
+  public ngAfterViewInit(): void {
+    this.#toastService.setViewContainerRef(this.toastContainer());
   }
 }
