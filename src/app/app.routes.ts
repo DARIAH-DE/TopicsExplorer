ab79f6405b82828f172fd9b6cd14a68763546499
severinsimmler <s.simmler@snapaddy.com>
2024-10-29 17:53:55 +0100

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { modelGuard } from './guards/model.guard';
+import { DocumentComponent, DocumentsComponent, SettingsComponent, TopicComponent, TopicsComponent } from './pages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: SettingsComponent },
+  { path: 'topics', component: TopicsComponent, canActivate: [modelGuard] },
+  { path: 'topics/:id', component: TopicComponent, canActivate: [modelGuard] },
+  { path: 'documents', component: DocumentsComponent, canActivate: [modelGuard] },
+  { path: 'documents/:id', component: DocumentComponent, canActivate: [modelGuard] },
+];
