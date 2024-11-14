c56131a63a09e3d4005bf556a0e3350f228d025b
severinsimmler <s.simmler@snapaddy.com>
2024-11-14 18:02:12 +0100

    feat: save model after each iteration

--- src/app/workers/topic-model.worker.ts
+++ src/app/workers/topic-model.worker.ts
@@ -1,6 +1,7 @@
 /// <reference lib="webworker" />
 
-import { TextCorpus, TopicModelOptions } from '../shared/interfaces.shared';
+import { db } from '../database/storage.database';
+import { TopicModelOptions } from '../shared/interfaces.shared';
 import { TopicModel } from '../shared/topic-model.shared';
 
 addEventListener('message', onTrainTopicModel);
@@ -10,14 +11,24 @@ addEventListener('message', onTrainTopicModel);
  *
  * @param message The message containing the text corpus and options.
  */
-export function onTrainTopicModel(message: MessageEvent<{ textCorpus: TextCorpus; options: TopicModelOptions }>): void {
-  const model = new TopicModel(message.data.textCorpus, message.data.options);
+export async function onTrainTopicModel(message: MessageEvent<{ options: TopicModelOptions }>): Promise<void> {
+  const [textDocuments, vocabulary] = await Promise.all([db.getTextDocuments(), db.getVocabulary()]);
+
+  if (!textDocuments || vocabulary.length === 0) {
+    return;
+  }
+
+  const model = new TopicModel()
+  model.setCorpus(textDocuments, vocabulary);
+  model.setHyperparameters(message.data.options.numTopics, message.data.options.alpha, message.data.options.beta);
 
   for (let i = 0; i < message.data.options.numIterations; i++) {
     model.update();
 
-    postMessage({ currentIteration: i + 1, topics: model.getTopics(5) });
+    await db.saveModel(model);
+
+    postMessage({ currentIteration: i + 1, isFinished: false });
   }
 
-  postMessage({ currentIteration: message.data.options.numIterations, model });
+  postMessage({ currentIteration: message.data.options.numIterations, isFinished: true });
 }
