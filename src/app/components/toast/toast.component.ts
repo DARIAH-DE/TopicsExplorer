afecc87ba636eb3571226e51ef5e09a290bc84b4
severinsimmler <s.simmler@snapaddy.com>
2025-09-11 17:22:53 +0200

    feat: toast

--- src/app/components/toast/toast.component.ts
+++ src/app/components/toast/toast.component.ts
@@ -1,42 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 
-type ToastType = 'info' | 'success' | 'warning' | 'danger';
+export type ToastType = 'info' | 'success' | 'warning' | 'danger';
 
 @Component({
   selector: 'app-toast',
   templateUrl: 'toast.component.html',
-  styleUrl: 'toast.component.scss',
-  standalone: true,
+  styleUrl: 'toast.component.css',
 })
 export class ToastComponent {
-  public message: string = '';
-  public type: ToastType = 'info';
+  public readonly message = input.required<string>();
+  public readonly type = input<ToastType>('info');
 
-  /**
-   * True if the toast is an info message.
-   */
-  public get isInfo(): boolean {
-    return this.type === 'info';
-  }
-
-  /**
-   * True if the toast is a success message.
-   */
-  public get isSuccess(): boolean {
-    return this.type === 'success';
-  }
-
-  /**
-   * True if the toast is a warning message.
-   */
-  public get isWarning(): boolean {
-    return this.type === 'warning';
-  }
-
-  /**
-   * True if the toast is a danger message.
-   */
-  public get isDanger(): boolean {
-    return this.type === 'danger';
-  }
+  public readonly isInfo = computed(() => this.type() === 'info');
+  public readonly isSuccess = computed(() => this.type() === 'success');
+  public readonly isWarning = computed(() => this.type() === 'warning');
+  public readonly isDanger = computed(() => this.type() === 'danger');
 }
