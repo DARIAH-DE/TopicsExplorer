6467a43febef4209c410026b11cfc18a2a3b10b2
severinsimmler <s.simmler@snapaddy.com>
2025-09-11 17:01:21 +0200

    feat: routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,8 @@
 import { Routes } from '@angular/router';
-import { modelGuard } from './guards/model.guard';
-import { DocumentComponent, DocumentsComponent, SettingsComponent, TopicComponent, TopicsComponent } from './pages';
+import { HomePage } from './pages/home/home.page';
+import { TrainingPage } from './pages/training/training.page';
 
 export const routes: Routes = [
-  { path: '', component: SettingsComponent },
-  { path: 'topics', component: TopicsComponent, canActivate: [modelGuard] },
-  { path: 'topics/:id', component: TopicComponent, canActivate: [modelGuard] },
-  { path: 'documents', component: DocumentsComponent, canActivate: [modelGuard] },
-  { path: 'documents/:id', component: DocumentComponent, canActivate: [modelGuard] },
+  { path: '', component: HomePage },
+  { path: 'training', component: TrainingPage },
 ];
