156bca56c0b7a238cecf98ad90652be70cc310b7
severinsimmler <s.simmler@snapaddy.com>
2025-09-11 17:22:22 +0200

    feat: navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,22 +1,25 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faDownload, faLayerGroup, faMoon, faRotateLeft, faSun, faUpload } from '@fortawesome/free-solid-svg-icons';
-import { ModelService } from '../../services/model.service';
+import { LdaService } from '../../services/lda.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss',
-  standalone: true,
+  styleUrl: './navbar.component.css',
   imports: [FaIconComponent],
 })
 export class NavbarComponent {
-  #theme: 'dark' | 'light' = 'dark';
-  #modelService = inject(ModelService);
-  #router = inject(Router);
+  readonly #ldaService = inject(LdaService);
+  readonly #router = inject(Router);
 
-  public readonly hasModel = this.#modelService.hasModel;
+  public readonly hasModel = this.#ldaService.hasModel;
+
+  public readonly theme = signal<'dark' | 'light'>('dark');
+
+  public readonly isLightTheme = computed(() => this.theme() === 'light');
+  public readonly isDarkTheme = computed(() => this.theme() === 'dark');
 
   public readonly faLayerGroup = faLayerGroup;
   public readonly faRotateLeft = faRotateLeft;
@@ -43,29 +46,14 @@ export class NavbarComponent {
    * Resets the current model.
    */
   public async clearModel(): Promise<void> {
-    await this.#modelService.clearModel();
     await this.#router.navigate(['/']);
   }
 
   /**
    * Toggles the theme between light and dark mode.
    */
-  public toggleTheme() {
-    this.#theme = this.isDarkTheme ? 'light' : 'dark';
-    document.getElementById('root')?.setAttribute('data-theme', this.#theme);
-  }
-
-  /**
-   * True if the current theme is dark.
-   */
-  public get isDarkTheme(): boolean {
-    return this.#theme === 'dark';
-  }
-
-  /**
-   * True if the current theme is light.
-   */
-  public get isLightTheme(): boolean {
-    return this.#theme === 'light';
+  public toggleTheme(): void {
+    this.theme.set(this.isDarkTheme() ? 'light' : 'dark');
+    document.getElementById('root')?.setAttribute('data-theme', this.theme());
   }
 }
