72c38287b41068cc47222afd77b8dc6f70a5ba91
severinsimmler <s.simmler@snapaddy.com>
2025-09-16 20:44:07 +0200

    feat: improve progress

--- src/app/pages/training/training.page.ts
+++ src/app/pages/training/training.page.ts
@@ -1,6 +1,7 @@
 import { Component, computed, inject, signal } from '@angular/core';
 import { NgxChartsModule, Series } from '@swimlane/ngx-charts';
 import { LdaService } from '../../services/lda.service';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-training',
@@ -10,10 +11,13 @@ import { LdaService } from '../../services/lda.service';
 })
 export class TrainingPage {
   readonly #ldaService = inject(LdaService);
+  readonly #themeService = inject(ThemeService);
 
-  public results = computed<Series[]>(() => [{ name: 'Progress', series: this.#ldaService.perplexityOverTime() }]);
+  public readonly results = computed<Series[]>(() => [{ name: '', series: this.#ldaService.perplexityOverTime() }]);
+  public readonly hasResults = computed(() => this.results().some((result) => result.series.length > 1));
 
   public readonly yAxisLabel = signal('Perplexity');
   public readonly xAxisLabel = signal('Iteration');
+  public readonly style = computed(() => `fill: ${this.#themeService.isDarkTheme() ? '#ffffff' : '#000000'};`);
   public readonly numIterations = this.#ldaService.numIterations;
 }
